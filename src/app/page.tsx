56661472759483334fd7a320178795a38b0abd70
NamDev <namnp.vnua@gmail.com>
2026-08-19 15:37:56 +0700

    load api bằng server hoặc browser (use Effect) đều bị call lại api khi UI được mounted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+// 'use client'
 import Link from "next/link";
 import AppTable from "@/components/app.table";
+// import { useEffect } from "react";
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch("http://localhost:8000/blogs");
+  const data = await res.json();
+  console.log(">>> Check res: ", data);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch("http://localhost:8000/blogs");
+  //     const data = await res.json();
+  //     console.log(">>> Check res: ", data);
+  //   };
+  //   fetchData();
+  // }, []);
+
   return (
     <div>
       <ul className="my-3">
@@ -13,7 +28,6 @@ export default function Home() {
         </li>
       </ul>
       <hr />
-
       <AppTable />
     </div>
   );
--- src/components/app.header.tsx
+++ src/components/app.header.tsx
@@ -3,7 +3,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Link from 'next/link';
 
 const AppHeader = () => {
@@ -14,15 +13,8 @@ const AppHeader = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} href="/">Home</Nav.Link>
-            <Nav.Link as={Link} href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item as={Link} href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item as={Link} href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item as={Link} href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={Link} href="/admin">Admin</Nav.Link>
+            <Nav.Link as={Link} href="/client">Client</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
